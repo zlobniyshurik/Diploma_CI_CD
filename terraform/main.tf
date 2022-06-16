2ca2697ffff2545266cdcd0b3e47e8579eae50fb
root <shurik@bfg-10k.ru>
2022-06-16 09:22:24 +0700

    Отделил переменные с ключами/явками/паролями от основного кода

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,25 +5,12 @@ terraform {
     }
   }
   required_version = ">=0.75.0"
-
-  backend "s3" {
-    endpoint   = "storage.yandexcloud.net"
-    bucket     = "zlobniy-shurik-terraform-storage"
-    region     = "ru-central1"
-    key        = "myterraform.tfstate"
-    access_key = "YCAJExo1efKl47MoZ2MK0DQBu"
-    secret_key = "YCNs1J-aIBklaRQOQDyC7S6JYlCF6Emt9CUjFjvb"
-
-    skip_region_validation      = true
-    skip_credentials_validation = true
-}
-
 }
 
 provider "yandex" {
-  token     = "AQAAAABa7AAAAATuwUw-hNWCZEangXG2yzDnLCQ"
-  cloud_id  = "b1gm2esngl1st6t66102"
-  folder_id = "b1g5mgpdiul101t5akk0"
-  zone      = "ru-central1-a"
+  token     = var.ya_token
+  cloud_id  = var.ya_cloud_id
+  folder_id = var.ya_folder_id
+  zone      = var.ya_zone
 }
 
---
+++ terraform/tfinit.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+terraform init -backend-config=backend.conf
