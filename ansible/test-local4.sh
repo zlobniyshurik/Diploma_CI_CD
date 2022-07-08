91157ef040f2651c7fdd9e2bfd40ad355d546f40
ZlobniyShurik <zlobniyshurik@gmail.com>
2022-07-08 16:42:17 +0700

    Удаляем локальные отладочные костыли

--- ansible/test-local4.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-#ANSIBLE_FORCE_COLOR=1 ansible-playbook -T 30 -i ./local-inventory ./tf_emulate.yml
-#ANSIBLE_FORCE_COLOR=1 ansible-playbook -T 30 -i ./local-inventory ./1_proxy.yml
-#ANSIBLE_FORCE_COLOR=1 ansible-playbook -T 30 -i ./local-inventory ./2_monitoring.yml
-#ANSIBLE_FORCE_COLOR=1 ansible-playbook -T 30 -i ./local-inventory ./3_web.yml
-ANSIBLE_FORCE_COLOR=1 ansible-playbook -T 30 -i ./local-inventory ./4_gitlab.yml
